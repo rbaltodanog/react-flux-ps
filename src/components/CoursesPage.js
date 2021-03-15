c6595ef631e1f8f9072cc49691ec2f5b8ad75e55
Roberto Baltodano <rbaltodanog@gmail.com>
2021-03-15 11:16:14 -0600

    Lesson 9 Changes

---
+++ src/components/CoursesPage.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
+
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    getCourses().then((_courses) => setCourses(_courses));
+  }, []);
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <Link className="btn btn-primary" to="/course">
+        Add Course
+      </Link>
+      <CourseList courses={courses} />
+    </>
+  );
+}
+
+export default CoursesPage;
